0bb0630b4ee3ecbec8eb8631a6e8936009e30a27
GetteAlan <alanexequielgette@gmail.com>
2020-06-21 20:03:24 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { HeaderComponent } from './components/header/header.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { PostingsComponent } from './components/postings/postings.component';
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FiltersComponent,
+    PostingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    FlexLayoutModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
